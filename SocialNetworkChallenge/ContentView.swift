5f127ef89874886c7745f869f9017481e269cf97
Emilly Maia <emillypereira1290@gmail.com>
2022-08-09 08:48:26 -0300

    Initial Commit

---
+++ SocialNetworkChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialNetworkChallenge
+//
+//  Created by Emilly Maia on 09/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialNetworkChallenge/SocialNetworkChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkChallengeApp.swift
+//  SocialNetworkChallenge
+//
+//  Created by Emilly Maia on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
